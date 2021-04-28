bed7aa163eb3fd7cb4ace64824f95ce360641a07
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2021-04-27 21:27:11 -0300

    C12 - For - Procurando o último número do Array

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -55,10 +55,20 @@ class CalcController {
     let last = this._operation.pop();
     let result = eval(this._operation.join(""));
     this._operation = [result, last];
+    this.setLastNumberToDisplay();
   }
 
   setLastNumberToDisplay(){
+    let lastNumber;
+    
+    for (let i = this._operation.length-1; i >= 0; i--){
+      if(!this.isOperator(this._operation[i])){
+        lastNumber = this._operation[i];
+        break;
+      }
+    }
 
+    this.displayCalc = lastNumber;
   }
 
   addOperation(value) {
@@ -69,6 +79,7 @@ class CalcController {
         console.log('Outra Coisa ->', value);
       } else {
         this.pushOperation(value);
+        this.setLastNumberToDisplay();
       }
     } else {
       if (this.isOperator(value)) {
